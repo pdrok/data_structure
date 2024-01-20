dc7b737b763bd0879e1480c68675581400d99947
Pdrok Lopez <cde.luis@gmail.com>
2024-01-20 04:30:54 -0300

    Add code with optimization examples

---
+++ usingPrintf.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+/* initialize the application */
+
+void int(void)
+{
+    int x;
+
+    x = *((int *) 0xbad1dea); /* if we are lucky, maybe the optimizer will remove it? */
+}
+int main(int argc, char **argv)
+{
+	init();
+
+#ifdef DEBUGGING_OUTPUT
+	/*
+	 * this type of debugging output is not recommended
+	 * but if you do it anyway:
+	 *
+	 * 1. Use stderr, which flushes automatically
+	 * 2. Be wary of buffered data on stdout.
+	 * 3. Wrap your debuggin statement in a #ifdef,
+	 *    so it is not active by default.
+	 */
+	fputs("Returned from init() in main()\n", stderr);
+#endif
+	return 0;
+}
