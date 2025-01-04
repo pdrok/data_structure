db0d4737a7a027c7bdbbf6cfcb531e1c67fcee84
Pedro Lopez <plopez@poweranalytics.com>
2025-01-04 07:51:04 -0300

    Converting to and from strings

---
+++ overflow.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* This program can be used to show how atoi etc. handle overflow. */
+/* fro example, try "overflow 1000000000000". */
+int main(int argc, char **argv)
+{
+    char c;
+    int i;
+    long l;
+    long long ll;
+
+    if(argc != 2) {
+	fprintf(stderr, "Usage: %s n\n", argv[0]);
+	return 1;
+    }
+
+    c = atoi(argv[1]);
+    i = atoi(argv[1]);
+    l = atoi(argv[1]);
+    ll = atoi(argv[1]);
+
+    printf("char: %d int: %d long: %ld long long: %lld", c, i, l, ll);
+
+    return 0;
+}
