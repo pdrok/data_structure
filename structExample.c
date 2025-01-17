f56e9db68fb9eb3546d0263a59409763780c6521
Pedro Lopez <plopez@poweranalytics.com>
2025-01-17 12:33:14 -0300

    Structs

---
+++ structExample.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+struct string {
+    int length;
+    char *data;
+};
+
+int main(int argc, char **argv)
+{
+    struct string s;
+
+    s.length = 4;
+    s.data = "this string is a lot longer than you think";
+
+    puts(s.data);
+
+    return 0;
+
+}
+
