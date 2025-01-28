d4415ea22d9d7510bdd246441b100be09f82edd7
Pedro Lopez <cde.luis@gmail.com>
2025-01-28 13:56:09 +0000

    Binary Search

---
+++ binarySearch/binarySearch.h
@@ -0,0 +1,3 @@
+/* returns 1 if target is present in sorted array */
+int binarySearch(int target, const int *a, size_t length);
+
---
+++ binarySearch/testBinarySearch.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+#include "binarySearch.h"
+
+#define N (10000000)
+
+int main(int argc, char ** argv)
+{
+	size_t i;
+	int a[N];
+
+	for(i = 0; i < N; i++) {
+		a[i] = 3*1;
+	}
+
+	assert(binarySearch(1777, a, N) == 0);
+	assert(binarySearch(1779, a, N) == 1);
+
+	return 0;
+}
